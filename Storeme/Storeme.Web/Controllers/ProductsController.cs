88a43174530525fa32e99dd07a95857c665305c3
Valentinles <valentinvn@abv.bg>
2022-08-17 11:37:04 +0300

    Intial commit

---
+++ Storeme/Storeme.Web/Controllers/ProductsController.cs
@@ -0,0 +1,123 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Storeme.Entities;
+using Storeme.Services.Contracts;
+using Storeme.Web.Models.Category;
+using Storeme.Web.Models.Product;
+
+namespace Storeme.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService productService;
+        private readonly ICategoryService categoryService;
+        private readonly IMapper mapper;
+        public ProductsController(IProductService productService, IMapper mapper, ICategoryService categoryService)
+        {
+            this.productService = productService;
+            this.mapper = mapper;
+            this.categoryService = categoryService;
+        }
+
+
+        public async Task<IActionResult> All(
+            string brand, string category, decimal priceFrom, decimal priceTo)
+        {
+            var products = await this.productService.GetAllProducts(brand, category, priceFrom, priceTo);
+
+            var categories = (await this.categoryService.GetCategories())
+                .Select(mapper.Map<CategoryListingViewModel>);
+
+            ViewData["Categories"] = new SelectList(categories, "Title", "Title");
+
+            var result = mapper.Map<IEnumerable<ProductListingViewModel>>(products);
+
+            return this.View(result);
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Add()
+        {
+            var categories = (await this.categoryService.GetCategories())
+                .Select(mapper.Map<CategoryListingViewModel>);
+
+            ViewData["Categories"] = new SelectList(categories, "Id", "Title");
+
+            return this.View();
+
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Add(ProductBindingModel model)
+        {
+
+            var product = mapper.Map<ProductBindingModel, Product>(model);
+
+            if (await this.productService.CreateProduct(product))
+            {
+                return RedirectToAction("All", "Products");
+            }
+
+            return RedirectToAction("Privacy", "Home");
+
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(int id)
+        {
+
+            if (await this.productService.DeleteProduct(id))
+            {
+                return RedirectToAction("All", "Products");
+            }
+
+            return RedirectToAction("Privacy", "Home");
+
+        }
+
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Update(int id)
+        {
+
+            var product = await this.productService.GetProduct(id);
+
+            var result = mapper.Map<ProductUpdateModel>(product);
+
+            var categories = (await this.categoryService.GetCategories())
+                .Select(mapper.Map<CategoryListingViewModel>);
+
+            ViewData["Categories"] = new SelectList(categories, "Id", "Title");
+
+            return this.View(result);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Update(int id, ProductUpdateModel model)
+        {
+            var product = mapper.Map<ProductUpdateModel, Product>(model);
+            if (await this.productService.UpdateProduct(product))
+            {
+                return RedirectToAction("All", "Products");
+            }
+            return RedirectToAction("Privacy", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+
+            var product = await this.productService.GetProduct(id);
+
+            var result = mapper.Map<ProductDetailsViewModel>(product);
+
+            return this.View(result);
+        }
+    }
+}
---
+++ Storeme/Storeme.Web/Controllers/WishlistController.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Storeme.Services.Contracts;
+using Storeme.Web.Models;
+using Storeme.Web.Models.Wishlist;
+
+namespace Storeme.Web.Controllers
+{
+    [Authorize]
+    public class WishlistController : Controller
+    {
+        private readonly IWishlistService wishlistService;
+        private readonly IMapper mapper;
+        public WishlistController(IWishlistService wishlistService, IMapper mapper)
+        {
+            this.wishlistService = wishlistService;
+            this.mapper = mapper;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddToWishlist(AddToWishlistBindingModel model)
+        {
+            var result = await this.wishlistService.AddItemToWishlist(model.ProductId, this.User.Identity.Name);
+            if (result)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return RedirectToAction("StoremeError", "Home",
+                new StoremeErrorViewModel() { Message = "Item already exists in your wishlist, if not please try again. Thank you!" });
+        }
+
+        [HttpPost]
+        [ActionName("AddToWishlistDetails")]
+        public async Task<IActionResult> AddToWishlist(int id)
+        {
+            var result = await this.wishlistService.AddItemToWishlist(id, this.User.Identity.Name);
+            if (result)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return RedirectToAction("StoremeError", "Home",
+                 new StoremeErrorViewModel() { Message = "Item already exists in your wishlist, if not please try again. Thank you!" });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveFromWishlist(RemoveFromWishlistBindingModel model)
+        {
+            var result = await this.wishlistService.RemoveItemFromWishlist(model.ProductId, this.User.Identity.Name);
+            if (result)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return RedirectToAction("Privacy", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> MyWishlist()
+        {
+            var wishlist = await this.wishlistService.UserWishlist(this.User.Identity.Name);
+
+            var items = mapper.Map<IEnumerable<WishlistItemViewModel>>(wishlist.Items);
+            var result = new WishlistListingViewModel()
+            {
+                Items = items
+            };
+            return this.View(result);
+        }
+
+        [HttpGet]
+        public async Task<int> WishlistCount()
+        {
+            var itemsCount = await this.wishlistService.WishlistItemsCount(this.User.Identity.Name);
+
+
+            return itemsCount;
+        }
+    }
+}
